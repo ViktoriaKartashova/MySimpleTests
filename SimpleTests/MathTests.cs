e2c92adedd691adef8d4e437c7dbe6e1ac357965
Карташова Виктория <donzovaola2007@gmail.com>
2026-04-16 21:58:32 +0800

    Добавьте файлы проекта.

---
+++ SimpleTests/MathTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework.Legacy;
+
+namespace SimpleTests
+{
+    [TestFixture]
+    public class MathTests
+    {
+        [Test]
+        public void Addition_2plus2_Returns4()
+        {
+            // ARRANGE
+            int a = 2;
+            int b = 2;
+            int expected = 4;
+
+            // ACT
+            int result = a + b;
+
+            // ASSERT
+            ClassicAssert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Subtraction_5minus3_Returns2()
+        {
+            int result = 5 - 3;
+            ClassicAssert.AreEqual(2, result);
+        }
+
+        [Test]
+        public void Multiplication_3times4_Returns12()
+        {
+            int result = 3 * 4;
+            ClassicAssert.AreEqual(12, result);
+        }
+    }
+}
